9e571b57cb5590fffaaa7b08601c491ec35a9e07
danielJot <jotibenti@gmail.com>
2025-06-18 18:16:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ function toggleMenu() {
 }
 
 function showTab(tabId) {
-  // Hide all tabs
   document.querySelectorAll('.tab').forEach(tab => {
     tab.classList.remove('active');
   });
-
-  // Show selected tab
   document.getElementById(tabId).classList.add('active');
-
-  // Close menu if open (mobile)
   document.getElementById("nav-list").classList.remove("show");
 }
